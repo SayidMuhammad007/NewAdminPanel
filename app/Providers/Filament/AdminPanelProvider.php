521df4067b4d269ed19189f525553c757abbdf30
SayidMuhammad007 <uzbekcoder507@gmail.com>
2025-07-26 15:33:15 +0500

    test

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('')
-            ->path('')
+            ->path('/')
             ->login()
             ->colors([
                 'primary' => Color::Teal,
